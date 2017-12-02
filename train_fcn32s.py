f0e3936377d9a4f91d4b6d81a4032911a4206f0c
Ji Han <33009871+j5207@users.noreply.github.com>
2017-12-02 00:44:27 -0500

    Update train_fcn32s.py

--- train_fcn32s.py
+++ train_fcn32s.py
@@ -47,7 +47,7 @@ def get_parameters(model, bias):
 
 def main():
     print 'start'
-    file = '/home/yaohuaxu1/fcn32s_from_caffe.pth'
+    file = '/home/yaohuaxu1/FCN/fcn32s_from_caffe.pth'
     model = fcn.models.FCN32s()
     model.load_state_dict(torch.load(file))
     model = model.cuda()
