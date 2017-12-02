26e67144d66fdb895aee55e78e6bfbe62967cbbd
Ji Han <33009871+j5207@users.noreply.github.com>
2017-12-02 17:04:53 -0500

    Update tester.py

--- fcn/tester.py
+++ fcn/tester.py
@@ -9,6 +9,22 @@ import torch.nn.functional as F
 import tqdm
 from PIL import Image
 
+def cross_entropy(input, target, weight=None, size_average=True):
+# input: (n, c, h, w), target: (n, h, w)
+    n, c, h, w = input.size()
+    # log_p: (n, c, h, w)
+    log_p = F.log_softmax(input)
+    # log_p: (n*h*w, c)
+    log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    log_p = log_p[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0]
+    log_p = log_p.view(-1, c)
+    # target: (n*h*w,)
+    mask = target >= 0
+    target = target[mask]
+    loss = F.nll_loss(log_p, target, weight=weight, size_average=False)
+    if size_average:
+        loss /= mask.data.sum()
+    return loss
 
 
 class Tester(object):
