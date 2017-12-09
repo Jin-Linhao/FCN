606452117baaf0dd9e3eca2b5de33cbe00ea7ed4
Ji Han <33009871+j5207@users.noreply.github.com>
2017-12-08 21:44:27 -0500

    Update tester.py

--- fcn/tester.py
+++ fcn/tester.py
@@ -105,6 +105,7 @@ class Tester(object):
             lbl_true = target.data.cpu()
             for img, lt, lp in zip(img, lbl_true, lbl_pred):
                 img, lt = self.untransform(img, lt)
+                skimage.io.imsave('img.jpg', img)
                 softmax = -F.log_softmax(score)
                 softmax = softmax.view((2,h,w)).data.cpu().numpy()
                 unary = softmax_to_unary(softmax)
